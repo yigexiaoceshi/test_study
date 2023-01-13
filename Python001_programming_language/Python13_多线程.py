f50a19dd1e67d9b1aa27baed5207f8923c2faa3b
李勇 <liyong@liyongdeMacBook-Air.local>
2023-01-13 22:27:52 +0800

    Python多线程&Python虚拟环境管理及pip依赖管理

--- Python001_programming_language/Python13_多线程.py
+++ Python001_programming_language/Python13_多线程.py
@@ -32,29 +32,29 @@ url_list = [("东北F4模仿秀.mp4", "https://aweme.snssdk.com/aweme/v1/playwm/
 二、一个进程里开启多个线程分别下载三个视频
 """
 
-
 # 定义一个下载视频的方法
-def task(name, url):
-    # 获取每个视频文件的文件流
-    response2 = requests.get(url)
-    # 创建本地文件，以传入的name参数进行命名，以别名file2打开
-    with open(name, mode='wb') as file2:
-        # 将获取到的视频文件的文件流写入到本地文件"name"当中
-        file2.write(response2.content)
-    # 打印每次视频下载完成的时间
-    print(f"视频 {name} 下载完成的时间为：{time.time()}")  # 3个线程全部执行完成总共差不多0.5秒左右；3个进程几乎是同一时间下载完3个视频
+# def task(name, url):
+#     # 获取每个视频文件的文件流
+#     response2 = requests.get(url)
+#     # 创建本地文件，以传入的name参数进行命名，以别名file2打开
+#     with open(name, mode='wb') as file2:
+#         # 将获取到的视频文件的文件流写入到本地文件"name"当中
+#         file2.write(response2.content)
+#     # 打印每次视频下载完成的时间
+#     print(f"视频 {name} 下载完成的时间为：{time.time()}")  # 3个线程全部执行完成总共差不多0.5秒左右；3个进程几乎是同一时间下载完3个视频
 
 
-# print("一个进程里开启多个线程分别下载三个视频")
+# print("一个进程里开启多个子线程分别下载三个视频")
 # print(f"多线程下载开始时间为：{time.time()}")
 # for name2, url2 in url_list:
-#     # 每次循环创建一个线程：threading.Thread(target=方法名或函数名,args=(参数1,参数2))，创建一个线程并将执行target里的方法作为任务分配给该线程
+#     # 每次循环创建一个子线程：threading.Thread(target=方法名或函数名,args=(参数1,参数2))，创建一个线程并将执行target里的方法作为任务分配给该线程
 #     thread = threading.Thread(target=task, args=(name2, url2))
 #     # 线程启动，调用task方法执行
 #     thread.start()
 # """
-# 结束时间为什么不能放在这，因为三次循环会迅速启动三个线程并执行，在start()之后会立马执行下面的print，
-# 而主线程则会等待三个子线程完全执行完成之后，程序才算是全部执行完成，从程序执行的结果里可以看到
+# 结束时间为什么不能放在这，因为程序开始运行时，系统会自动创建一个进程，进程里创建一个主线程，由主线程自上而下按顺序执行代码，
+# 遇到子线程创建并且执行任务时，主线程会继续向下执行，并不会停下等待子线程执行完成，而是执行完所有代码之后才会等子线程执行结束，
+# 此时程序才算是全部执行完成并且终止程序执行，从程序执行的结果里可以看到
 # """
 # print(f"end_time为：{time.time()}")  # 创建线程并启动线程的循环执行完成后，立马会执行该print，此时子线程还没执行完成
 
@@ -72,18 +72,80 @@ for name3,url3 in url_list:
     process = multiprocessing.Process(target=task,args=(name3,url3))
     process.start()
 """
-if __name__ == "__main__":
-    # 记录视频开始下载的时间
-    print(f"开始下载视频的时间为：{time.time()}")
-    for name3, url3 in url_list:
-        # 每次循环创建一个进程(每个进程会自动创建一个线程，由线程来执行target里的方法)
-        process = multiprocessing.Process(target=task, args=(name3, url3))
-        # 启动进程里的线程开始执行task方法
-        process.start()
+# if __name__ == "__main__":
+#     # 记录视频开始下载的时间
+#     print(f"开始下载视频的时间为：{time.time()}")
+#     for name3, url3 in url_list:
+#         # 每次循环创建一个进程(每个进程会自动创建一个线程，由线程来执行target里的方法)
+#         process = multiprocessing.Process(target=task, args=(name3, url3))
+#         # 启动进程里的线程开始执行task方法
+#         process.start()
 
 """
 小结：
 通过多线程或多进程都能提升效率，多进程对于系统资源的消耗更大，至于什么场景下使用多进程，什么场景下使用多线程，须了解Python的GIL锁机制
 """
-# GIL锁
-a = 123
+"""
+GIL锁(全局解释器锁：Global Interpreter Lock)，是CPython解释器特有的一个东西，让一个进程同一时刻只能有一个线程可以被CPU调用
+    * 如果想利用计算机CPU的多核优势，让CPU同时处理一些任务，适合用多进程开发（即使资源消耗较大），GIL锁不住进程之间的并发能力
+    * 如果程序不利用计算机的多核优势，适合用多线程开发
+常见的程序开发中，计算操作需要使用CPU的多核优势，IO操作不需要利用CPU的多核优势：
+    * 计算密集型，用多进程开发，例如：大量的数据计算（累加计算示例）
+    * IO密集型，用多线程开发，例如：文件读写、爬虫、网络数据传输（下载抖音视频示例）
+"""
+
+
+# 累加计算示例（计算密集型）
+# 1、串行处理
+# start = time.time()
+# result = 0
+# for i in range(100000000):
+#     result += i
+# print(f"result等于：{result}")
+# end_time = time.time()
+# print(f"计算耗时为：", end_time - start)  # 总消耗的时间为15.8秒
+
+
+# 2、使用多进程处理
+# def task1(start_num, end_num, queue):
+#     result0 = 0
+#     for j in range(start_num, end_num):
+#         result0 += j
+#     queue.put(result0)
+#
+#
+# if __name__ == "__main__":
+#     queue = multiprocessing.Queue()
+#     start_time = time.time()
+#     process1 = multiprocessing.Process(target=task1, args=(0, 50000000, queue))
+#     process1.start()
+#     process2 = multiprocessing.Process(target=task1, args=(50000000, 100000000, queue))
+#     process2.start()
+#     result1 = queue.get(block=True)
+#     result2 = queue.get(block=True)
+#     print("一亿数字累加的结果为：", result1 + result2)
+#     end_time = time.time()
+#     print("一亿数字累加执行的总时长为：", end_time - start_time)  # 约为5.06秒
+
+# 在程序开发过程中，多线程和多进程是可以相互结合使用，比如创建2个进程（进程数建议和CPU核心个数相同），每个进程创建3个线程
+def thread_task():
+    pass
+
+
+def task2(start, end):
+    thread1 = threading.Thread(target=thread_task)
+    thread1.start()
+    thread2 = threading.Thread(target=thread_task)
+    thread2.start()
+    thread3 = threading.Thread(target=thread_task)
+    thread3.start()
+
+
+if __name__ == "__main__":
+    start_time1 = time.time()
+    process3 = multiprocessing.Process(target=task2, args=(0, 50000000))
+    process3.start()
+    process4 = multiprocessing.Process(target=task2, args=(50000000, 100000000))
+    process4.start()
+    end_time1 = time.time()
+    print("一亿数字累加执行的总时长为：", end_time1 - start_time1)  # 0.03秒内
--- Python001_programming_language/Python15_pip依赖管理和虚拟环境.py
+++ Python001_programming_language/Python15_pip依赖管理和虚拟环境.py
@@ -1,1 +1,48 @@
 # -*-coding: utf-8 -*-
+"""
+pip介绍：pip是Python中的标准库管理器，允许安装和管理不属于Python标准库里的其他软件包
+www.pypi.com托管了大量非常流行的库
+在Python2的2.7.9以及Python3的3.4版本往后，pip被直接包括在Python的安装包里
+常用方法：
+ * pip help：查看帮助文档
+ * pip install 包名：安装包（默认最新版本），如果需要安装指定版本则执行pip install 包名==版本号
+ * pip install -U 包名：升级包的版本到最新
+ * pip uninstall 包名：卸载
+ * pip list：列出所有的包文件
+ * pip download 包名：下载安装包
+ * pip search 包名：搜索安装包
+不少国外的镜像文件下载速度较慢甚至无法连接，这里可以使用国内的镜像源下载安装:
+pip3 install 包名 -i 国内镜像源地址 --trusted-host 国内镜像源的host
+示例：pip install jupyter -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com
+国内常用pip镜像源：
+阿里云：https://mirrors.aliyun.com/pypi/simple/
+清华：https://pypi.tuna.tsinghua.deu.cn/simple
+豆瓣：http://pypi.douban.com/simple
+"""
+
+"""
+虚拟环境管理：
+1、命令行操作：
+    * 创建一个名称为test_venv的虚拟环境：python -m venv test_venv
+    * 当前目录下生成"test_venv"目录，可使用ls查看：ls
+    * 进入到test_venv目录：cd test_venv
+    * 激活并进入虚拟环境：
+        Windows：tutorial-env/Scripts/activate.bat
+        Unix或者macOS：source tutorial-env/bin/activate
+    * 激活之后会自动进入虚拟环境目录(进入虚拟环境的命令行前面带虚拟环境的名称，本机的系统环境是不带的)，查看虚拟环境下安装的包：pip list
+    * 退出虚拟环境：deactivate
+    * 删除虚拟环境：rm -rf test_venv
+2、pycharm操作：
+    * 在pycharm的欢迎界面点击Create New Project，进入虚拟环境创建页面
+    * 在Location输入框里输入项目的路径及名称
+    * 在New Virtualenv environment里操作虚拟环境的创建及设置
+    * New environment using下拉有Virtualenv、Pipenv、Conda三种方式，默认为Virtualenv
+    * New environment using下的Location指的是虚拟环境的路径及名称
+    * Base interpreter指的是虚拟环境指定的Python解释器（可能会有多个版本，指定哪个版本则虚拟环境会自动创建一套该版本的Python开发环境）
+    * Inherit global site-packages意思是继承全局的packages，勾选的话会将系统安装的所有包同步到该虚拟环境下使用，不勾选则是全新的虚拟环境
+    * Make available to all projects意思是该虚拟环境对所有项目可用，一般不勾选
+    * Existing interpreter勾选该项意思是使用已经存在的环境，点击下拉选择即可
+    注1：全新的虚拟环境默认只包含pip(标准库管理工具)、setuptools(设置工具)、urllib3(网络请求库)三个包，其他的需另行安装
+    注2：pycharm创建的虚拟环境界面，可以复制虚拟环境的路径，在命令行同样可以进行进入虚拟环境、进入python命令行、退出虚拟环境、包管理等操作
+附：pycharm设置页面，Project Interpreter页面的Project Interpreter下拉选择所有的环境，包括所有的虚拟环境和系统环境，可管理所有环境下所有的包
+"""
